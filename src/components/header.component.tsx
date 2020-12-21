8c9653513789aff6e1e233d247f904806926f29a
Veras <sabrina.alves@drcash.com.br>
2020-12-21 17:05:03 -0300

    add navbar component

---
+++ src/components/header.component.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+// styles
+import * as Styled from './header.styles';
+
+// images
+const logo = require('../icons/logo.svg') as string;
+
+const HeaderComponent: React.FunctionComponent = () => {
+  return (
+    <Styled.Header>
+      <Styled.NavBar>
+        <img src={logo} alt="logo" />
+      </Styled.NavBar>
+    </Styled.Header>
+  );
+};
+
+export default HeaderComponent;
---
+++ src/components/header.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Header = styled.section`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: #ffffff;
+`;
+
+export const NavBar = styled.nav`
+  display: flex;
+  width: 1400px;
+  padding: 0 20px;
+  margin: 0 auto;
+
+`;
