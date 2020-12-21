9739284d03537463fac2b3aa47401eea363b6c89
Veras <sabrina.alves@drcash.com.br>
2020-12-21 17:50:56 -0300

    add title component into home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -5,12 +5,14 @@ import * as Styled from './home.styles';
 
 // components
 import Header from '../../components/header/header.component';
+import TitleComponent from '../../components/title/title.component';
 import CardComponent from '../../components/card/card.component';
 
 const HomePage: React.FunctionComponent = () => {
   return (
     <Styled.Container>
       <Header />
+      <TitleComponent />
       <CardComponent />
     </Styled.Container>
   );
