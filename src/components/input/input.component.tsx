af3b38f0b8b742218525d921166d5c0c9e421c98
Veras <sabrina.alves@drcash.com.br>
2020-12-22 16:09:14 -0300

    add only number input

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -4,10 +4,21 @@ import * as React from 'react';
 import * as Styled from './input.styles';
 
 const InputComponent: React.FunctionComponent = () => {
+  const handleInput: React.MouseEventHandler = event => {
+    event.preventDefault();
+
+    let value = event.target.value;
+    value = value.replace(/\D/g, '');
+    value = value.replace(/(\d)(\d{3})$/, '$1,$2');
+    value = value.replace(/(?=(\d{3})+(\D))\B/g, ',');
+
+    event.currentTarget.value = value;
+  };
+
   return (
     <>
       <Styled.Label>Total amount</Styled.Label>
-      <Styled.InputIconDollarSign />
+      <Styled.InputIconDollarSign onChange={handleInput} />
     </>
   );
 };
