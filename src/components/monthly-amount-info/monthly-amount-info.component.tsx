226295abe281f337a0e4880d05bd15f5a0be7a4d
Veras <sabrina.alves@drcash.com.br>
2020-12-27 17:48:37 -0300

    add total month data

--- src/components/monthly-amount-info/monthly-amount-info.component.tsx
+++ src/components/monthly-amount-info/monthly-amount-info.component.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { DateState } from '../../redux/date/date.reducer';
 import { DateTypes } from '../../redux/date/date.types';
+import { TotalAmountState } from '../../redux/total-amount/total-amount.reducer';
 
 // styles
 import * as Styled from './monthly-amount-info.styles';
@@ -23,21 +24,22 @@ const MonthlyAmountInfoComponent: React.FunctionComponent = () => {
     'December'
   ];
   const date = useSelector<DateState, DateTypes>(state => state.date);
+  const value = useSelector<TotalAmountState, string>(state => state.value);
 
   return (
     <Styled.MonthlyAmountInfo>
       <Styled.MonthlyAmount>
         <h3>Monthly amount</h3>
-        <h1>$521</h1>
+        <h1>${value}</h1>
       </Styled.MonthlyAmount>
       <Styled.Detail>
         <p>
           {/* eslint-disable-next-line react/no-unescaped-entities */}
-          You're planning <span> 48 monthly deposits </span>to reach your{' '}
+          You're planning <span> {date.date.totalMonth} monthly deposits </span>to reach your{' '}
           <span> U$ 25,000 </span>goal by
           <span>
             {' '}
-            {monthsOfTheYear[date.month]} {date.year}.
+            {monthsOfTheYear[date.date.month]} {date.date.year}.
           </span>
         </p>
       </Styled.Detail>
