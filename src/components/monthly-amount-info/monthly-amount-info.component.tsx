a7e6e313b00f43378f6607adf1a74c8691457623
Veras <sabrina.alves@drcash.com.br>
2020-12-29 15:56:12 -0300

    add totalAmountState to monthly amount info component

--- src/components/monthly-amount-info/monthly-amount-info.component.tsx
+++ src/components/monthly-amount-info/monthly-amount-info.component.tsx
@@ -3,10 +3,10 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { DateState } from '../../redux/date/date.reducer';
 import { DateTypes } from '../../redux/date/date.types';
-import { TotalAmountState } from '../../redux/total-amount/total-amount.reducer';
 
 // styles
 import * as Styled from './monthly-amount-info.styles';
+import { TotalAmountState } from '../../redux/total-amount/total-amount.reducer';
 
 const MonthlyAmountInfoComponent: React.FunctionComponent = () => {
   const monthsOfTheYear = [
@@ -24,19 +24,22 @@ const MonthlyAmountInfoComponent: React.FunctionComponent = () => {
     'December'
   ];
   const date = useSelector<DateState, DateTypes>(state => state.date);
-  const value = useSelector<TotalAmountState, string>(state => state.value);
+  const totalAmount = useSelector<TotalAmountState, TotalAmountState['value']>(
+    state => state.value
+  );
 
   return (
     <Styled.MonthlyAmountInfo>
       <Styled.MonthlyAmount>
         <h3>Monthly amount</h3>
-        <h1>${value}</h1>
+        {console.log('total amount', totalAmount)}
+        <h1>${(25000 / date.date.totalMonth).toFixed(0)}</h1>
       </Styled.MonthlyAmount>
       <Styled.Detail>
         <p>
           {/* eslint-disable-next-line react/no-unescaped-entities */}
-          You're planning <span> {date.date.totalMonth} monthly deposits </span>to reach your{' '}
-          <span> U$ 25,000 </span>goal by
+          You're planning <span> {date.date.totalMonth} monthly deposits </span>
+          to reach your <span> U$ {totalAmount} </span>goal by
           <span>
             {' '}
             {monthsOfTheYear[date.date.month]} {date.date.year}.
